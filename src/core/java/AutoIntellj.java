bdaa273617cac7f2352ceacda6448552b1b62480
harmandeepsingh <harmandeepsinghclass@gmail.com>
2016-12-12 01:53:57 -0500

    Adding a Simple class for test.

---
+++ src/core/java/AutoIntellj.java
@@ -0,0 +1,12 @@
+package core.java;
+
+/**
+ * Created by Harman on 12/12/2016.
+ */
+public class AutoIntellj {
+    public static void main(String[] args) {
+
+        int number =10;
+        System.out.println(number);
+    }
+}
